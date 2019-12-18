46650f543c9d16d798a7891d8950daf557d9c681
Bjorn Coltof <bjorn.coltof@gmail.com>
2019-12-18 11:46:44 +0100

    Added me some material

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatSliderModule} from '@angular/material/slider';
+
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        MatToolbarModule,
+        MatSliderModule
+      ]
     }).compileComponents();
   }));
 
@@ -19,13 +27,13 @@ describe('AppComponent', () => {
   it(`should have as title 'time-tracker'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('time-tracker');
+    expect(app.title).toEqual('Time Tracker');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('time-tracker app is running!');
+    expect(compiled.querySelector('mat-toolbar.main-toolbar > span').textContent).toContain('Time Tracker');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatSliderModule} from '@angular/material/slider';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
